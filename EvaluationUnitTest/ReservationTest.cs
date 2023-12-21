b757062d6b56aea3a7da6339dce2085e4fe221bf
Tess <fchlstess@gmail.com>
2023-12-21 16:47:30 +0100

    Correcting errors

--- EvaluationUnitTest/ReservationTest.cs
+++ EvaluationUnitTest/ReservationTest.cs
@@ -3,12 +3,12 @@
 namespace EvaluationUnitTest
 {
     [TestClass]
-    public class Reservation
+    public class ReservationTests
     {
         [TestMethod]
         public void Reservation_WhenCalled_SetsMadeByProperty()
         {
-            User user = new User("Remi Theo");
+            User user = new User();
             Reservation reservation = new Reservation(user);
             Assert.AreEqual(user, reservation.MadeBy);
         }
